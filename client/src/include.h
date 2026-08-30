5f4379a939c87cc289888031ac85eb7ae5d3ef94
Berat Vargeloglu <68159722+Berat-Vargeloglu@users.noreply.github.com>
2026-08-30 18:13:09 +0300

    [update] Update and rename structure.h to include.h

---
+++ client/src/include.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <fcntl.h>
+#include <io.h>
+#include <Lmcons.h>
+
+#pragma comment(lib, "ws2_32.lib")
