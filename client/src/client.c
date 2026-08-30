f27a21fac1ddc832dd603648401dc8070270ca9a
Berat Vargeloglu <68159722+Berat-Vargeloglu@users.noreply.github.com>
2026-08-30 18:13:57 +0300

    [update] updated client.c

--- client/src/client.c
+++ client/src/client.c
@@ -1,25 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
-#include <fcntl.h>
-#include <io.h>
-#include <Lmcons.h>
-
-#pragma comment(lib, "ws2_32.lib")
-
-#define IP_LEN 20 
-#define PORT_BUFFER_LEN 12
-#define INPUT_BUFFER_LEN 100
-// More byte for memory alignment, tolerance and future.
-
-typedef enum {
-	INPUT_CANCELED = -1,
-	INPUT_SUCCESS = 0,
-	INPUT_TOO_LONG = 1
-} InputStatus;
+#include "utils.h"
 
 #define BUFFER_SIZE 1024
 
@@ -262,10 +241,6 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-InputStatus get_string(char *buffer, int max_len);
-int get_custom_ip(char *ip);
-int get_custom_port(u_short *port);
-
 int main(void) {
 
 	char SERVER_IP[IP_LEN] = "127.0.0.1";
@@ -387,73 +362,4 @@ int main(void) {
 	WSACleanup();
 
 	return EXIT_SUCCESS;
-}
-
-int get_string(char *buffer, int max_len) {
-	if (fgets(buffer, max_len, stdin) == NULL) {
-		return INPUT_CANCELED;
-	}
-
-	if (strchr(buffer, '\n') == NULL) {
-		int c;
-		while ((c = getchar()) != '\n' && c != EOF);
-		return INPUT_TOO_LONG;
-	}
-
-	buffer[strcspn(buffer, "\n")] = '\0';
-	return INPUT_SUCCESS;
-}
-
-int get_custom_ip(char *ip) {
-	while (1) {
-		printf("\nEnter IP Address: ");
-
-		int ip_status = get_string(ip, IP_LEN);
-
-		if (ip_status == INPUT_CANCELED) {
-			printf("\nInput canceled. Exiting...\n");
-			return INPUT_CANCELED;
-		}
-		else if (ip_status == INPUT_TOO_LONG) {
-			printf("\nError: Input is too long for an IP address! Please try again.\n");
-			continue;
-		}
-
-		struct in_addr sa;
-		if (inet_pton(AF_INET, ip, &sa) == 1) {
-			return INPUT_SUCCESS;
-		}
-		else {
-			printf("\nError: Invalid IP address format! Please try again.");
-		}
-	}
-}
-
-int get_custom_port(u_short *port) {
-	char port_buffer[PORT_BUFFER_LEN];
-	while (1) {
-		printf("Enter Port (1-65535): ");
-
-		int port_status = get_string(port_buffer, sizeof(port_buffer));
-
-		if (port_status == INPUT_CANCELED) {
-			printf("\nInput canceled. Exiting...\n");
-			return INPUT_CANCELED;
-		}
-		else if (port_status == INPUT_TOO_LONG) {
-			printf("\nError: Input is too long for a port number! Please try again.\n");
-			continue;
-		}
-
-		char *endptr;
-		long val = strtol(port_buffer, &endptr, 10);
-
-		if (endptr != port_buffer && *endptr == '\0' && (val >= 1 && val <= 65535)) {
-			*port = (u_short)val;
-			return INPUT_SUCCESS;
-		}
-		else {
-			printf("\nError: Invalid port number! Must be an integer between 1 and 65535.\n");
-		}
-	}
 }
