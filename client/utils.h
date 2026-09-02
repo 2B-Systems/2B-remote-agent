5f43917c0db4e7b9a1e9bc4b3e9cbc1487aadecb
Berat Vargeloglu <68159722+Berat-Vargeloglu@users.noreply.github.com>
2026-09-02 20:34:19 +0300

    [refactor] Refactor utils.h for Unicode support

--- client/utils.h
+++ client/utils.h
@@ -2,17 +2,14 @@
 #include "include.h"
 
 // Global Variables
-
 extern SOCKET sock;
 
 // Macros
-
 #define IP_LEN 20 
 #define PORT_BUFFER_LEN 12
 #define INPUT_BUFFER_LEN 100
 
 // Data Structures
-
 typedef enum {
 	INPUT_CANCELED = -1,
 	INPUT_SUCCESS = 0,
@@ -20,10 +17,9 @@ typedef enum {
 } InputStatus;
 
 // Prototypes
-
-InputStatus get_string(char *buffer, int max_len);
-int get_custom_ip(char *ip);
+InputStatus get_string(wchar_t *buffer, int max_len);
+int get_custom_ip(wchar_t *ip);
 int get_custom_port(u_short *port);
-int get_server_config(char *ip_buffer, u_short *port);
-SOCKET connect_to_server(const char *ip, u_short port);
+int get_server_config(wchar_t *ip_buffer, u_short *port);
+SOCKET connect_to_server(const wchar_t *ip, u_short port);
 void run_message_loop();
